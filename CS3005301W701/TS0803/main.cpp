6f45ef3b0ae403450dbb9ced135310fa8babbed4
aifred0729-TW <aifred0729tw@gmail.com>
2024-04-12 23:35:13 +0800

    Finish Memory Simulator

--- CS3005301W701/TS0803/main.cpp
+++ CS3005301W701/TS0803/main.cpp
@@ -3,27 +3,29 @@
 #include "memory.h"
 #include <string>
 
-void runProcess(Memory mem);
-bool runSecurityCheckInterface(Memory mem, int point, int dataType, int dataSize);
+void runProcess(int memsize, int count);
+int getDataType(std::string dataType);
+void runSecurityCheckInterface(Memory &mem, int point, int dataType, int dataSize);
 
 int main() {
 	using namespace std;
 
 	int memsize = 0;
-	cin >> memsize;
-	Memory mem(memsize);
-
 	int count = 0;
+
+	cin >> memsize;
 	cin >> count;
 
-	for (unsigned int i = 0; i < count; i++) {
-		runProcess(mem);
-	}
+
+	runProcess(memsize, count);
+
 	return 0;
 }
 
-void runProcess(Memory mem) {
+void runProcess(int memsize, int count) {
 	using namespace std;
+
+	Memory mem(memsize);
 
 	string opcode = "";
 	string typeOpcode = "";
@@ -32,18 +34,22 @@ void runProcess(Memory mem) {
 	int dataType = 0;
 	string data = "";
 
-	cin >> opcode >> pointer >> typeOpcode;
+	for (unsigned int i = 0; i < count; i++) {
+		cin >> opcode >> pointer >> typeOpcode;
 
-	dataType = getDataType(typeOpcode);
+		dataType = getDataType(typeOpcode);
 
-	if (opcode == "Set") {
-		getline(cin >> ws, data);
-		if (!runSecurityCheckInterface(mem, pointer, dataType, data.size())) return;
-
+		if (opcode == "Set") {
+			getline(cin >> ws, data);
+			runSecurityCheckInterface(mem, pointer, dataType, data.size());
+			mem.write(pointer, dataType, data);
+		}
+		else if (opcode == "Get") {
+			runSecurityCheckInterface(mem, pointer, dataType, 0);
+			mem.read(pointer, dataType);
+		}
 	}
-	else if (opcode == "Get") {
-		if (!runSecurityCheckInterface(mem, pointer, dataType, 0)) return;
-	}
+
 	return;
 }
 
@@ -55,11 +61,10 @@ int getDataType(std::string dataType) {
 	return 0;
 }
 
-bool runSecurityCheckInterface(Memory mem, int point, int dataType, int dataSize) {
+void runSecurityCheckInterface(Memory &mem, int point, int dataType, int dataSize) {
 	using namespace std;
 	if (mem.runSecurityCheck(point, dataType, dataSize)) {
 		cout << "Violation Access." << endl;
-		return false;
 	}
-	return true;
+	return;
 }
--- CS3005301W701/TS0803/memory.h
+++ CS3005301W701/TS0803/memory.h
@@ -4,10 +4,11 @@
 class Memory {
 private:
 	// char 1, short 2, int 3, string 4
-	std::vector<int> space;
+	std::vector<unsigned char> space;
 	std::vector<int> locate;
 	std::vector<int> type;
 	int size;
+
 public:
 	Memory(int memsize) {
 		size = memsize;
@@ -15,4 +16,6 @@ public:
 	}
 
 	bool runSecurityCheck(int point, int type, int strSize);
+	void write(int pointer, int dataType, std::string data);
+	void read(int pointer, int dataType);
 };
