d8e08f8ffd5e14f856868cc0112706ff696b3ef2
aifred0729-TW <aifred0729tw@gmail.com>
2024-05-07 04:20:40 +0800

    Fix last commit coding style

--- CS3005301W1101/TS1101/Email.h
+++ CS3005301W1101/TS1101/Email.h
@@ -1,15 +1,15 @@
 #pragma once
 #include "Document.h"
 
-class Email : public Document {
+class Email: public Document {
     private:
     std::string title  = "";
     std::string sender = "";
     std::string recver = "";
 
     public:
-    Email(std::string doc, std::string s, std::string r, std::string t) : Document(doc) {
-        title = t;
+    Email(std::string doc, std::string s, std::string r, std::string t): Document(doc) {
+        title  = t;
         sender = s;
         recver = r;
     }
--- CS3005301W1101/TS1101/File.h
+++ CS3005301W1101/TS1101/File.h
@@ -1,15 +1,15 @@
 #pragma once
 #include "Document.h"
 
-class File : public Document {
+class File: public Document {
     private:
     std::string pathname = "";
 
     public:
-    File(std::string doc, std::string path) : Document(doc) {    
+    File(std::string doc, std::string path): Document(doc) {
         pathname = path;
     }
 
-    void setPathname(std::string path);
+    void        setPathname(std::string path);
     std::string getPathname();
 };
